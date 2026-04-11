d27123ac0d27ff0f09e953c74e728386debfce67
PRASHANT KAURAI <153915493+prashantkaurai@users.noreply.github.com>
2026-04-11 11:21:58 +0530

    Refactor motion.img components for better readability

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -12,44 +12,24 @@ const Brands = () => {
       <div className="container py-14">
         <div className="flex flex-wrap justify-center  lg:justify-between gap-6  ">
           <motion.img
-            variants={SlideLeft(0.2)}
-            initial="initial"
-            whileInView={"animate"}
-            src={Brand1}
-            alt="brand"
-            className="w-[110px] md:w-[200px] "
+            variants={SlideLeft(0.2)} initial="initial" whileInView={"animate"}
+            src={Brand1} alt="brand" className="w-[110px] md:w-[200px] "
           />
           <motion.img
-            variants={SlideLeft(0.4)}
-            initial="initial"
-            whileInView={"animate"}
-            src={Brand2}
-            alt="brand"
-            className="w-[110px] md:w-[200px]"
+            variants={SlideLeft(0.4)} initial="initial" whileInView={"animate"}
+            src={Brand2} alt="brand" className="w-[110px] md:w-[200px]"
           />
           <motion.img
-            variants={SlideLeft(0.6)}
-            initial="initial"
-            whileInView={"animate"}
-            src={Brand3}
-            alt="brand"
-            className="w-[110px] md:w-[200px]"
+            variants={SlideLeft(0.6)} initial="initial" whileInView={"animate"}
+            src={Brand3} alt="brand" className="w-[110px] md:w-[200px]"
           />
           <motion.img
-            variants={SlideLeft(0.8)}
-            initial="initial"
-            whileInView={"animate"}
-            src={Brand4}
-            alt="brand"
-            className="w-[110px] md:w-[200px]"
+            variants={SlideLeft(0.8)} initial="initial" whileInView={"animate"}
+            src={Brand4} alt="brand" className="w-[110px] md:w-[200px]"
           />
           <motion.img
-            variants={SlideLeft(1.0)}
-            initial="initial"
-            whileInView={"animate"}
-            src={Brand5}
-            alt="brand"
-            className="w-[110px] md:w-[200px]"
+            variants={SlideLeft(1.0)} initial="initial" whileInView={"animate"}
+            src={Brand5} alt="brand" className="w-[110px] md:w-[200px]"
           />
         </div>
       </div>
