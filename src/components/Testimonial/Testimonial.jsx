43891c443854424c8e6e52e7e3dcd6dda2a1ace1
PRASHANT KAURAI <153915493+prashantkaurai@users.noreply.github.com>
2026-04-13 15:43:55 +0530

    Refactor motion component props for consistency

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -34,17 +34,13 @@ const Testimonial = () => {
       {/* heading title */}
       <div className="space-y-4 text-center max-w-[550px] mx-auto mb-8">
         <motion.h1
-          variants={SlideUp(0.2)}
-          initial="initial"
-          whileInView="animate"
+          variants={SlideUp(0.2)} initial="initial" whileInView="animate"
           className="text-4xl font-bold font-serif"
         >
           Words from our coustomers
         </motion.h1>
         <motion.p
-          variants={SlideUp(0.4)}
-          initial="initial"
-          whileInView="animate"
+          variants={SlideUp(0.4)} initial="initial" whileInView="animate"
           className="text-gray-500 text-sm max-w-[350px] mx-auto"
         >
           Bring your dream home to life with one-on-one design help & hand
@@ -57,18 +53,13 @@ const Testimonial = () => {
           {TestimonialData.map((card) => {
             return (
               <motion.div
-                variants={SlideLeft(card.delay)}
-                initial="initial"
-                whileInView="animate"
-                key={card.id}
+                variants={SlideLeft(card.delay)} initial="initial" whileInView="animate" key={card.id}
                 className="border-[1px] border-gray-500 px-5 py-10 text-white group hover:bg-white duration-300"
               >
                 {/* Upper section */}
                 <div className="flex flex-row items-center gap-3 ">
                   <img
-                    src={card.img}
-                    alt=""
-                    className="w-[60px] rounded-full"
+                    src={card.img} alt="" className="w-[60px] rounded-full"
                   />
                   <div>
                     <p className="text-sm font-bold group-hover:text-black">
